446822688b4374ccd1ebabce6ce5030e21cc83f1
Yong.Yang <yong.yang@Runmit.com>
2015-11-13 18:36:04 +0800

    init啊：

--- Assets/test.cs
+++ Assets/test.cs
@@ -28,7 +28,9 @@ public class test : MonoBehaviour {
 
 	public void bluetoothClick(GameObject obj)
 	{
-
+		if (bluetooth.GetComponent<UIToggle>().value) {
+				
+		}
 	}
 
 	public void testClick(GameObject obj)
@@ -39,5 +41,54 @@ public class test : MonoBehaviour {
 	public void TTTTClick(GameObject button) {
 		Debug.Log("test....." + packFeild.GetComponent<UIInput>().value);
 		Debug.Log("test 23 " + bluetooth.GetComponent<UIToggle>().value);
+
+	}
+
+	public void openFile()
+	{
+		{
+			OpenFileName ofn = new OpenFileName();
+			
+			ofn.structSize = Marshal.SizeOf(ofn);
+			
+			ofn.filter = "JPG Files (*.jpg)\0*.jpg\0All Files (*.*)\0*.*\0";
+			
+			ofn.file = new string(new char[256]);
+			
+			ofn.maxFile = ofn.file.Length;
+			
+			ofn.fileTitle = new string(new char[64]);
+			
+			ofn.maxFileTitle = ofn.fileTitle.Length;
+			
+			ofn.initialDir =UnityEngine.Application.dataPath;//默认路径
+			
+			ofn.title = "打开文件";
+			
+			ofn.defExt = "JPG";//显示文件的类型
+			//注意 一下项目不一定要全选 但是0x00000008项不要缺少
+			ofn.flags=0x00080000|0x00001000|0x00000800|0x00000200|0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST| OFN_ALLOWMULTISELECT|OFN_NOCHANGEDIR
+			
+			if(DllTest.GetOpenFileName( ofn ))
+			{
+				
+				StartCoroutine(WaitLoad(ofn.file));//加载图片到panle
+				
+				Debug.Log( "Selected file with full path: {0}"+ofn.file );
+				
+			}
+		}
+	}
+
+	//加载图片
+	IEnumerator WaitLoad(string fileName)
+	{
+		WWW wwwTexture=new WWW("file://"+fileName);
+		
+		Debug.Log(wwwTexture.url);
+		
+		yield return wwwTexture;
+		
+		plane.renderer.material.mainTexture=wwwTexture.texture;
 	}
 }
